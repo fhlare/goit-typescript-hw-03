a9e164eb1a0cb1309ced23cf33683188a019eb37
fhlare <127776539+fhlare@users.noreply.github.com>
2024-01-05 03:41:16 +0300

    end hw-03

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+  
+  getSignature():number {
+    return this.signature;
+  }
+}
+
+class Person{
+  constructor(private key: Key) { }
+  
+  getKey(): Key{
+    return this.key;
+  }
+}
+
+abstract class House{
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key){}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Welcome! Door is open!");
+    } else {
+      console.log("Door is closed!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +49,5 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
+
